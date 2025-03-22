418a2ae4ce9fc37de71bf4cff2cbb37b1d2ec8b0
Troy Mulder <126816710+funcodelic@users.noreply.github.com>
2025-03-22 17:19:02 -0400

    Initial Commit

---
+++ VitalShift/VitalShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitalShiftApp.swift
+//  VitalShift
+//
+//  Created by Troy Mulder on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct VitalShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
